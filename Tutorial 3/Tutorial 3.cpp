419ba55aade8eefe08d0681ecbe6034af4a2e3f5
Joses <joses.chan@gmail.com>
2022-05-07 00:03:30 +0100

    Updating kernels and implementation

--- Tutorial 3/Tutorial 3.cpp
+++ Tutorial 3/Tutorial 3.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 
 #include "Utils.h"
+#include "CImg.h"
+
+using namespace cimg_library;
 
 void print_help() {
 	std::cerr << "Application usage:" << std::endl;
@@ -16,6 +19,7 @@ int main(int argc, char **argv) {
 	//Part 1 - handle command line options such as device selection, verbosity, etc.
 	int platform_id = 0;
 	int device_id = 0;
+	string image_filename= "test.ppm";
 
 	for (int i = 1; i < argc; i++)	{
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
@@ -188,105 +192,8 @@ int main(int argc, char **argv) {
 	catch (cl::Error err) {
 		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
 	}
-	catch (const cl::Error& err) {
-		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
-	}
 	catch (CImgException& err) {
 		std::cerr << "ERROR: " << err.what() << std::endl;
 	}
-
-
 	return 0;
-}
-
-
-kernel void identity(global const uchar* A, global uchar* B) {
-	int id = get_global_id(0);
-	B[id] = A[id];
-}
-
-kernel void filter_r(global const uchar* A, global uchar* B) {
-	int id = get_global_id(0);
-	int image_size = get_global_size(0) / 3; //each image consists of 3 colour channels
-	int colour_channel = id / image_size; // 0 - red, 1 - green, 2 - blue
-
-	//this is just a copy operation, modify to filter out the individual colour channels
-	if (colour_channel == 0) {
-		B[id] = A[id];
-	}
-
-}
-
-//simple ND identity kernel
-kernel void identityND(global const uchar* A, global uchar* B) {
-	int width = get_global_size(0); //image width in pixels
-	int height = get_global_size(1); //image height in pixels
-	int image_size = width * height; //image size in pixels
-	int channels = get_global_size(2); //number of colour channels: 3 for RGB
-
-	int x = get_global_id(0); //current x coord.
-	int y = get_global_id(1); //current y coord.
-	int c = get_global_id(2); //current colour channel
-
-	int id = x + y * width + c * image_size; //global id in 1D space
-
-	B[id] = A[id];
-}
-
-//2D averaging filter
-kernel void avg_filterND(global const uchar* A, global uchar* B) {
-	int width = get_global_size(0); //image width in pixels
-	int height = get_global_size(1); //image height in pixels
-	int image_size = width * height; //image size in pixels
-	int channels = get_global_size(2); //number of colour channels: 3 for RGB
-
-	int x = get_global_id(0); //current x coord.
-	int y = get_global_id(1); //current y coord.
-	int c = get_global_id(2); //current colour channel
-
-	int id = x + y * width + c * image_size; //global id in 1D space
-
-	uint result = 0;
-
-	//simple boundary handling - just copy the original pixel
-	if ((x == 0) || (x == width - 1) || (y == 0) || (y == height - 1)) {
-		result = A[id];
-	}
-	else {
-		for (int i = (x - 1); i <= (x + 1); i++)
-			for (int j = (y - 1); j <= (y + 1); j++)
-				result += A[i + j * width + c * image_size];
-
-		result /= 9;
-	}
-
-	B[id] = (uchar)result;
-}
-
-//2D 3x3 convolution kernel
-kernel void convolutionND(global const uchar* A, global uchar* B, constant float* mask) {
-	int width = get_global_size(0); //image width in pixels
-	int height = get_global_size(1); //image height in pixels
-	int image_size = width * height; //image size in pixels
-	int channels = get_global_size(2); //number of colour channels: 3 for RGB
-
-	int x = get_global_id(0); //current x coord.
-	int y = get_global_id(1); //current y coord.
-	int c = get_global_id(2); //current colour channel
-
-	int id = x + y * width + c * image_size; //global id in 1D space
-
-	float result = 0;
-
-	//simple boundary handling - just copy the original pixel
-	if ((x == 0) || (x == width - 1) || (y == 0) || (y == height - 1)) {
-		result = A[id];
-	}
-	else {
-		for (int i = (x - 1); i <= (x + 1); i++)
-			for (int j = (y - 1); j <= (y + 1); j++)
-				result += A[i + j * width + c * image_size] * mask[i - (x - 1) + j - (y - 1)];
-	}
-
-	B[id] = (uchar)result;
 }
